d58013c260f061bad954e910b4692beb026ebdc1
Karnati Karunya Kanth <23pa1a05a6@vishnu.edu.in>
2026-05-05 21:40:59 +0530

    Update SQL queries and format in queries.sql

--- Week1/day1/queries.sql
+++ Week1/day1/queries.sql
@@ -1,2 +1,28 @@
-q1
+Q1
 select * from Employee
+Q2
+select name, salary from Employee
+Q3
+select * from Employee where age>30
+Q4
+select name from Department
+Q5
+select e.name from Employee e 
+join Department d 
+ON e.department_id = d.department_id
+where d.name ='IT'
+Q6
+select * from Employee
+where name like 'J%'
+Q7
+select * from Employee
+where name like '%e'
+Q8
+select * from Employee
+where name like '%a%'
+Q9
+select * from Employee
+where LENGTH(name)=9
+Q10
+select * from Employee
+where name like '_o%'
